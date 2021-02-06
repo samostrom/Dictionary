ac6a88277c503dc4528df59528bc98a73d1bb716
Sam Ostrom <sostrom@gmail.com>
2021-02-05 17:22:17 -0800

    test for github 2 factor auth

--- input.py
+++ input.py
@@ -1,10 +1,16 @@
 import json
+import difflib
 
 data = json.load(open("data.json"))
 
 def translate(str):
-    return data[str]  
+    if str in data:
+        return data[str] 
+    else:
+        return "Word is not in the dictionary" 
 
-word = input("Enter word:")
+word = input("Enter word:").lower()
 
 print(translate(word))
+
+test
